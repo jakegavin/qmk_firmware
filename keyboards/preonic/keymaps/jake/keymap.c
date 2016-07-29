e3977f4beb9a7aaf62e219deb52c17870cf8199b
Jake Gavin <jakegavin@gmail.com>
2016-07-29 11:04:49 -0700

    add navigation layer

--- keyboards/preonic/keymaps/jake/keymap.c
+++ keyboards/preonic/keymaps/jake/keymap.c
@@ -11,6 +11,7 @@
 // entirely and just use numbers.
 #define _QWERTY 0
 #define _COLEMAK 1
+#define _NAV 2
 #define _LOWER 3
 #define _RAISE 4
 #define _ADJUST 16
@@ -18,6 +19,7 @@
 enum preonic_keycodes {
   QWERTY = SAFE_RANGE,
   COLEMAK,
+  NAV,
   LOWER,
   RAISE,
   BACKLIT
@@ -39,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Shift |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Lower| Ctrl | GUI  | Alt  |Space |Enter | Del  | Bksp | Alt  | GUI  | Ctrl |Raise |
+ * | Lower| Nav  | GUI  | Alt  |Space |Enter | Del  | Bksp | Alt  | GUI  | Nav  |Raise |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = {
@@ -47,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_PIPE},
   {KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
   {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT},
-  {LOWER,   KC_LCTL, KC_LGUI, KC_LALT, KC_SPC,  KC_ENT,  KC_DEL,  KC_BSPC, KC_RALT, KC_RGUI, KC_RCTL, RAISE  }
+  {LOWER,   NAV,     KC_LGUI, KC_LALT, KC_SPC,  KC_ENT,  KC_DEL,  KC_BSPC, KC_RALT, KC_RGUI, NAV,     RAISE  }
 },
 
 /* Colemak
@@ -69,6 +71,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_ESC,  KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT},
   {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT },
   {BACKLIT, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
+},
+
+/* NAV
+ * ,-----------------------------------------------------------------------------------.
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |      | Play | Vol- | Vol+ | Next |      |      |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * |      |      |      |      |      |      | Left | Down |  Up  |Right |      |      |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |             |      |      |      |      |      |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_NAV] = {
+  {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
+  {_______, _______, _______, _______, _______, _______, KC_MPLY, KC_VOLD, KC_VOLU, KC_MNXT, _______, _______},
+  {_______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______},
+  {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
+  {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
 },
 
 /* Lower
@@ -175,6 +198,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
               PLAY_NOTE_ARRAY(tone_colemak, false, 0);
             #endif
             persistant_default_layer_set(1UL<<_COLEMAK);
+          }
+          return false;
+          break;
+        case NAV:
+          if (record->event.pressed) {
+            layer_on(_NAV);
+          } else {
+            layer_off(_NAV);
           }
           return false;
           break;
